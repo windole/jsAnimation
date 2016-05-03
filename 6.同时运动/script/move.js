97c77f37b121fa22cf18e687e6b499989b7b0490
windole <1322670109@qq.com>
2016-05-03 20:07:19 +0800

    多属性同时运动

---
+++ 6.同时运动/script/move.js
@@ -0,0 +1,47 @@
+//startMove(obj,{attr1:itarget1,attr2:itarget2},fn)
+function startMove(obj,json,fn){  //添加一个回调函数fn
+  clearInterval(obj.timer);//1.2+++
+  obj.timer=setInterval(function(){//1.2+++
+  var flag;
+  //1.取当前的值
+  for(var attr in json){
+  flag=true;
+  var icur=null;
+  if(attr=='opacity'){
+    icur=Math.round(parseFloat(getStyle(obj,attr))*100);
+    }else{
+      icur=parseInt(getStyle(obj,attr));
+      }
+//2.算速度
+    var speed=(json[attr]-icur)/8;
+    speed=speed>0?Math.ceil(speed):Math.floor(speed);
+//3.检测停止
+    if(icur!=json[attr]){
+        flag=false;
+    }
+
+      if(attr=='opacity'){
+          obj.style.filter='alpha(opacity:'+(icur+speed)+')';
+          obj.style.opacity=(icur+speed)/100;
+      }else{
+      obj.style[attr]=icur+speed+'px';
+      }
+
+    if(flag){
+        clearInterval(obj.timer);
+      if(fn){ //判断是否存在回调函数,并调用
+        fn();
+          }
+    }
+    }
+  
+  },30);
+
+}
+function getStyle(obj,attr){
+    if(obj.currentStyle){
+        return obj.currentStyle[attr];
+      }else{
+        return getComputedStyle(obj,false)[attr];
+        }
+}
